a9e6ca4efc91dffe167ea23daa51684de0a4db91
Mohammad Tourjoman <mtourjoman01@hotmail.com>
2019-03-31 15:13:31 -0700

    Initial Commit

---
+++ MenuBoard/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MenuBoard
+//
+//  Created by Mohammad Tourjoman on 3/31/19.
+//  Copyright © 2019 Mohammad Tourjoman. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ MenuBoardTests/MenuBoardTests.swift
@@ -0,0 +1,34 @@
+//
+//  MenuBoardTests.swift
+//  MenuBoardTests
+//
+//  Created by Mohammad Tourjoman on 3/31/19.
+//  Copyright © 2019 Mohammad Tourjoman. All rights reserved.
+//
+
+import XCTest
+@testable import MenuBoard
+
+class MenuBoardTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
